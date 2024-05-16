efcedd3178872e41305db0b6169d56757d8027e4
bruno <bruno@brunos-MacBook-Air.local>
2024-05-16 12:04:57 -0300

    Testando o github actions

--- build.sh
+++
@@ -1,4 +0,0 @@
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 175014665151.dkr.ecr.us-east-1.amazonaws.com
-docker buildx build --platform linux/amd64 -t github-action
-docker tag github-action:latest 175014665151.dkr.ecr.us-east-1.amazonaws.com/github-action:latest
-docker push github-action:latest 175014665151.dkr.ecr.us-east-1.amazonaws.com/github-action:latest
